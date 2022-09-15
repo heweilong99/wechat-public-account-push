29b402e2895f7e817fe65db4832122b32d07e832
heweilong99 <108136608+heweilong99@users.noreply.github.com>
2022-09-15 21:42:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "1f1493660767377b0abfd510c963f939",
+  APP_SECRET: "	qe13n8tFLioAtW9acMWoUkv7V-RXkUUfwfcunJuPX0o",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -25,7 +25,7 @@ export const config = {
       // 想要发送的人的名字
       name: "傻仔",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oh4IX6M2OjdHkU9Pi8-WisIHi6AY",
+      id: "oh4IX6M2OjdHkU9Pi8-WisIHi6AY,oh4IX6OnX_FVlUiUPO_7O_rSUnCA,oh4IX6IWipqmR2XH-zz5LD7nSAbk",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "10txr8oXEqnBkvBq2gCyCHcnLxKqr7jC9Tapxw9Lomc",
       // 所在省份
